27a1577e5802c4f1fa13940ac828a04710e06258
Mikael Dúi Bolinder <github@mikaelbolinder.se>
2019-07-07 23:30:30 +0200

    Moved the sorting method to the sorter class.

--- src/TrackerList.Base/Sorting/TrackerSorter.cs
+++ src/TrackerList.Base/Sorting/TrackerSorter.cs
@@ -6,5 +6,18 @@ namespace TrackerList.Sorting
 {
     public class TrackerSorter
     {
+        public static IDictionary<TrackerSortDefinition, Tracker[]> SortTrackers(IList<Tracker> trackers, IEnumerable<TrackerSortDefinition> trackerSortDefinitions)
+        {
+            // Sort the trackers according to some definition.
+
+            Dictionary<TrackerSortDefinition, Tracker[]> sortations = new Dictionary<TrackerSortDefinition, Tracker[]>();
+
+            foreach (var sortDefinition in trackerSortDefinitions)
+            {
+                sortations.Add(sortDefinition, trackers.Where(t => sortDefinition.ValidationStatement.Invoke(t)).ToArray());
+            }
+
+            return sortations;
+        }
     }
 }
--- src/TrackerList.Git/TrackerStorage.cs
+++ src/TrackerList.Git/TrackerStorage.cs
@@ -8,19 +8,7 @@ namespace TrackerList.Git
 {
     public class GitTrackerStorage : ITrackerStorage
     {
-        public static IDictionary<TrackerSortDefinition, Tracker[]> SortTrackers(IList<Tracker> trackers, IEnumerable<TrackerSortDefinition> trackerSortDefinitions)
-        {
-            // Sort the trackers according to some definition.
 
-            Dictionary<TrackerSortDefinition, Tracker[]> sortations = new Dictionary<TrackerSortDefinition, Tracker[]>();
-
-            foreach(var sortDefinition in trackerSortDefinitions)
-            {
-                sortations.Add(sortDefinition, trackers.Where(t => sortDefinition.ValidationStatement.Invoke(t)).ToArray());
-            }
-
-            return sortations;
-        }
 
         public ITracker[] GetAllTrackers()
         {
